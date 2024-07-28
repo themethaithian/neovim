d8df0e033bbd7590b134952fce43a37324791749
thaithanach.t <thaithanach.t@arise.tech>
2024-07-29 02:10:48 +0700

    feat: add option

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -9,3 +9,6 @@ vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<Leader>h', ':noh<CR>')
+
