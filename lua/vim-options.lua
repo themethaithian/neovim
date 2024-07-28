58af1c959d40b47c729649ab2853f4eb39a14bbe
thaithanach.t <thaithanach.t@arise.tech>
2024-07-28 18:40:43 +0700

    feat: tmux navigator

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,8 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.cmd("set relativenumber")
+
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
