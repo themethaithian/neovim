b6d79b92e74a0d83b6594968f6640263ca432069
thaithanach.t <thaithanach.t@arise.tech>
2024-07-28 17:48:32 +0700

    feat: debugging

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,18 +4,20 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
-        null_ls.builtins.diagnostics.golangci_lint,    -- go
-        null_ls.builtins.formatting.black,             -- python
-        null_ls.builtins.formatting.isort,             -- python
-        null_ls.builtins.formatting.prettier,          -- javascript
-        null_ls.builtins.formatting.stylua,            -- lua
-        null_ls.builtins.formatting.gofmt,             -- go
-        null_ls.builtins.formatting.gofumpt,           -- go
-        null_ls.builtins.formatting.goimports,         -- go
+        null_ls.builtins.diagnostics.golangci_lint, -- go
+        null_ls.builtins.formatting.black,         -- python
+        null_ls.builtins.formatting.isort,         -- python
+        null_ls.builtins.formatting.prettier,      -- javascript
+        null_ls.builtins.formatting.stylua,        -- lua
+        null_ls.builtins.formatting.gofmt,         -- go
+        null_ls.builtins.formatting.gofumpt,       -- go
+        null_ls.builtins.formatting.goimports,     -- go
         null_ls.builtins.formatting.goimports_reviser, -- go
       },
     })
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    vim.keymap.set("n", "<leader>gf", function()
+      vim.lsp.buf.format({ timeout_ms = 2000 })
+    end, {})
   end,
 }
