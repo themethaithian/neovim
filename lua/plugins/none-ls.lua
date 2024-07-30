b1495e85fc69a1da7bffe0f220b67386f6a5fe66
thaithanach.t <thaithanach.t@arise.tech>
2024-07-30 18:08:19 +0700

    feat: update none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,7 +9,7 @@ return {
         null_ls.builtins.formatting.isort,         -- python
         null_ls.builtins.formatting.prettier,      -- javascript
         null_ls.builtins.formatting.stylua,        -- lua
-        null_ls.builtins.formatting.gofmt,         -- go
+        -- null_ls.builtins.formatting.gofmt,         -- go
         null_ls.builtins.formatting.gofumpt,       -- go
         null_ls.builtins.formatting.goimports,     -- go
         null_ls.builtins.formatting.goimports_reviser, -- go
